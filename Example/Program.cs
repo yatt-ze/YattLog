75b000efb2779cfa620b1897cc04f03ccf7e446d
yatt-ze <arthurhuia@gmail.com>
2023-10-24 20:13:28 +1300

    Added Comments

--- Example/Program.cs
+++ Example/Program.cs
@@ -14,33 +14,44 @@ namespace Example
             PathFileExample();
         }
 
+        /// <summary>
+        /// Wont do anything
+        /// </summary>
         private static void NoneExample()
         {
-            Config stdConfig = new Config
+            Config noneConfig = new Config
             {
                 Level = LogLevel.Debug,
                 Target = LogTarget.None
             };
-            Logger stdLog = new Logger(stdConfig);
+            Logger noneLog = new Logger(noneConfig);
 
-            stdLog.Debug("This is a none debug log");
-            stdLog.Info("This is a none info log");
-            stdLog.Warn("This is a none warn log");
-            stdLog.Error("This is a none error log");
-            stdLog.Fatal("This is a none fatal log");
+            noneLog.Debug("This is a none debug log");
+            noneLog.Info("This is a none info log");
+            noneLog.Warn("This is a none warn log");
+            noneLog.Error("This is a none error log");
+            noneLog.Fatal("This is a none fatal log");
         }
 
+        /// <summary>
+        /// Uses the default configuration
+        /// Target = Std
+        /// Level = Info
+        /// </summary>
         private static void DefaultExample()
         {
-            Logger stdLog = new Logger();
+            Logger defaultLog = new Logger();
 
-            stdLog.Debug("This is a default debug log");
-            stdLog.Info("This is a default info log");
-            stdLog.Warn("This is a default warn log");
-            stdLog.Error("This is a default error log");
-            stdLog.Fatal("This is a default fatal log");
+            defaultLog.Debug("This is a default debug log");
+            defaultLog.Info("This is a default info log");
+            defaultLog.Warn("This is a default warn log");
+            defaultLog.Error("This is a default error log");
+            defaultLog.Fatal("This is a default fatal log");
         }
 
+        /// <summary>
+        /// Logs to std with a debug level
+        /// </summary>
         private static void StdExample()
         {
             Config stdConfig = new Config
@@ -57,37 +68,45 @@ namespace Example
             stdLog.Fatal("This is a std fatal log");
         }
 
+        /// <summary>
+        /// Logs to file in the default location (<Exe Location>/Logs/<date>.log)
+        /// Level = Debug
+        /// </summary>
         private static void DefaultFileExample()
         {
-            Config stdConfig = new Config
+            Config dFileConfig = new Config
             {
                 Level = LogLevel.Debug,
                 Target = LogTarget.File
             };
-            Logger stdLog = new Logger(stdConfig);
+            Logger dFileLog = new Logger(dFileConfig);
 
-            stdLog.Debug("This is a default file debug log");
-            stdLog.Info("This is a default file info log");
-            stdLog.Warn("This is a default file warn log");
-            stdLog.Error("This is a default file error log");
-            stdLog.Fatal("This is a default file fatal log");
+            dFileLog.Debug("This is a default file debug log");
+            dFileLog.Info("This is a default file info log");
+            dFileLog.Warn("This is a default file warn log");
+            dFileLog.Error("This is a default file error log");
+            dFileLog.Fatal("This is a default file fatal log");
         }
 
+        /// <summary>
+        /// Logs to file in a specified location
+        /// Level = Debug
+        /// </summary>
         private static void PathFileExample()
         {
-            Config stdConfig = new Config
+            Config pFileConfig = new Config
             {
                 Level = LogLevel.Debug,
                 Target = LogTarget.File,
                 Path = Path.Combine(Directory.GetCurrentDirectory(), "Yatt.log")
             };
-            Logger stdLog = new Logger(stdConfig);
+            Logger pFileLog = new Logger(pFileConfig);
 
-            stdLog.Debug("This is a file debug log with a specified path");
-            stdLog.Info("This is a file info log with a specified path");
-            stdLog.Warn("This is a file warn log with a specified path");
-            stdLog.Error("This is a file error log with a specified path");
-            stdLog.Fatal("This is a file fatal log with a specified path");
+            pFileLog.Debug("This is a file debug log with a specified path");
+            pFileLog.Info("This is a file info log with a specified path");
+            pFileLog.Warn("This is a file warn log with a specified path");
+            pFileLog.Error("This is a file error log with a specified path");
+            pFileLog.Fatal("This is a file fatal log with a specified path");
         }
     }
 }
